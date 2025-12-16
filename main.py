b94413cc2adf36ff4ea4b129445f60b885fc84e0
zahreddine <zahrdinh@gmail.com>
2025-12-16 10:25:44 +0100

    implementation/test insertion notif

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, HTTPException
 from Insertions import Appareil, CreationRequest, Equipement, Evenement, Lieu, Notification, Utilisateur, inserer_appareil, inserer_equipement, inserer_lieu, inserer_notification, inserer_utilisateur, insertion_evenement
+from selection import *
 
 app = FastAPI()
 
@@ -43,3 +44,31 @@ def create_evenement(evenement : Evenement):
 def create_notification(notif : Notification): 
     notification_id= inserer_notification(notif)
     return{"status": "success", "utilisateur_id": notification_id}
+
+
+@app.get("/chercher_utilisateur/{utilisateur_id}")
+def get_utilisateur(utilisateur_id: str):
+    utilisateur = select_utilisateur(utilisateur_id)
+
+    if utilisateur is None:
+        raise HTTPException(status_code=404, detail="Utilisateur introuvable")
+
+    return utilisateur
+
+@app.get("/chercher_lieu/{utilisateur_id}")
+def get_lieu(utilisateur_id: str):
+    lieu = select_lieu(utilisateur_id)
+
+    if lieu is None:
+        raise HTTPException(status_code=404, detail="lieu introuvable")
+
+    return lieu
+
+@app.get("/chercher_notification/{utilisateur_id}")
+def get_notification(utilisateur_id: str):
+    lieu = select_notification(utilisateur_id)
+
+    if lieu is None:
+        raise HTTPException(status_code=404, detail="lieu introuvable")
+
+    return lieu
